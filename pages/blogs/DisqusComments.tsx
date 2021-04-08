4a5c9c00ba1ce6e8ca7602896c5ff49273a8203f
Irtisam24 <irtisam.d@gmail.com>
2021-04-08 14:18:51 +0500

    changed getstaticpaths to getserversideprops

--- pages/blogs/DisqusComments.tsx
+++ pages/blogs/DisqusComments.tsx
@@ -1,15 +1,19 @@
 import { DiscussionEmbed } from "disqus-react";
 const DisqusComments = ({ post }) => {
-  const disqusShortname = "hashigoc";
-  const disqusConfig = {
-    url: `https://www.hashigoc.com/blogs/${post.slug}/`,
-    identifier: post.slug,
-    title: post.blog_title,
-  };
-  return (
-    <div>
-      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
-    </div>
-  );
+  try {
+    const disqusShortname = "hashigoc";
+    const disqusConfig = {
+      url: `https://www.hashigoc.com/blogs/${post.slug}/`,
+      identifier: post.slug,
+      title: post.blog_title,
+    };
+    return (
+      <div>
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+      </div>
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 export default DisqusComments;
